b68b971041515a18914774062171cb608d3479b8
Magret <belloabiodun17@gmail.com>
2025-03-08 08:58:53 -0330

    Implemented endpoint to get all users

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -3,4 +3,7 @@ import * as userController from '../controllers/user-controller.js';
 
 const router = express.Router();
 
+router.route("/")
+    .get(userController.index);
+
 export default router;
