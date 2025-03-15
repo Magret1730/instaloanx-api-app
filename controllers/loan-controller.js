2b7bceddbce9a036d3b37ad54d398e27a75b7ad4
Magret <belloabiodun17@gmail.com>
2025-03-14 23:06:47 -0230

    Implement route for users to apply loan

--- controllers/loan-controller.js
+++ controllers/loan-controller.js
@@ -18,4 +18,50 @@ const index = async (_req, res) => {
     }
 };
 
-export { index}
+
+const applyLoan = async (req, res) => {
+    try {
+        const { amount, purpose } = req.body;
+
+        // loan purposes
+        const purposes = ["Education", "Business", "Medical", "Personal", "Other"];
+
+        // Check if user has an active loan
+        const activeLoan = await knex("loans")
+            .where({ user_id: req.user.id, status: "Active" || "Pending" })
+            .first();
+
+        if (activeLoan) {
+            return res.status(400).json({ message: "You already have an active loan." });
+        }
+
+        // Validations
+        if (!amount || isNaN(amount) || Number(amount) <= 0) {
+            return res.status(400).json({ message: "Invalid loan amount" });
+        }
+
+        if (!purpose || typeof purpose !== "string" || !purposes.includes(purpose)) {
+            return res.status(400).json({ message: "Invalid loan purpose" });
+        }
+
+        // Insert loan application
+        const [newLoanId] = await knex("loans")
+            .insert({
+                user_id: req.user.id,
+                loan_amount: amount,
+                loan_purpose: purpose,
+                status: "Pending", // Default status
+            });
+
+        // Fetch the newly inserted user
+        const newLoan = await knex("loans").where({ id: newLoanId }).first();
+        // console.log(newLoan);
+
+        res.status(201).json({ success: true, data: newLoan });
+    } catch (error) {
+        console.error("Loan application error:", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+export { index, applyLoan }
--- routes/loans-routes.js
+++ routes/loans-routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as loanController from '../controllers/loan-controller.js';
+import { authorization } from '../middlewares/authorization.js';
 
 const router = express.Router();
 
-router.route("/")
-    .get(loanController.index);
+router.route("/").get(authorization, loanController.index);
+router.route("/applyLoan").post(authorization, loanController.applyLoan);
 
 // router.route("/:id")
 //     .get(loanController.findLoanPerUser );
