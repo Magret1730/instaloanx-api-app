0a18aef19a581ef285f2f640bef0d25944c0868d
Magret <belloabiodun17@gmail.com>
2025-03-19 22:49:48 -0230

    Refactor loan status based on remaining balance

--- seed-data/loans.js
+++ seed-data/loans.js
@@ -2,7 +2,7 @@ export default [
     {
         user_id: 1,
         loan_amount: 5000.0,
-        remaining_balance: 5000.0,
+        remaining_balance: 0.0,
         loan_purpose: 'Home renovation',
         status: 'Fully Repaid',
     },
@@ -16,7 +16,7 @@ export default [
     {
         user_id: 4,
         loan_amount: 7500.0,
-        remaining_balance: 7500.0,
+        remaining_balance: 0.0,
         loan_purpose: 'Education fees',
         status: 'Fully Repaid',
     },
